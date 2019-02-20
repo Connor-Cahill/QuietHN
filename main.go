51ef7ade64e8eb20eb8d4b23789212023c261804
Connor Cahill <connorcahill913@gmail.com>
2019-02-19 21:21:37 -0800

    [Added]: now makes requests and updates cache in background so load speeds stay very good.

--- main.go
+++ main.go
@@ -32,9 +32,30 @@ func main() {
 }
 
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+	sc := storyCache{
+		numStories: numStories,
+		duration:   3 * time.Second,
+	}
+
+	//	acts as ticker and every n seconds we update our cache
+	go func() {
+		ticker := time.NewTicker(3 * time.Second)
+		for {
+			temp := storyCache{
+				numStories: numStories,
+				duration:   6 * time.Second,
+			}
+			temp.stories()
+			sc.mutex.Lock()
+			sc.cache = temp.cache
+			sc.expiration = temp.expiration
+			sc.mutex.Unlock()
+			<-ticker.C
+		}
+	}()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		stories, err := getCachedStories(numStories)
+		stories, err := sc.stories()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -50,27 +71,27 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	})
 }
 
-var (
-	cache           []item
-	cacheExpiration time.Time
-	cacheMutex      sync.Mutex
-)
+type storyCache struct {
+	numStories int
+	cache      []item
+	expiration time.Time     //	expiration of cache
+	duration   time.Duration //	duration of how long cache is good for
+	mutex      sync.Mutex    //	mutex to stop race conditions amongst go routines
+}
 
-func getCachedStories(numStories int) ([]item, error) {
-	cacheMutex.Lock()         //	Locks so no other go routines be happenin
-	defer cacheMutex.Unlock() //	when function exits, unlocks mutext
-	//	if cache is not expired just return cache
-	if time.Now().Sub(cacheExpiration) < 0 {
-		return cache, nil
+func (sc *storyCache) stories() ([]item, error) {
+	sc.mutex.Lock()         //	Locks mutex so no other go routines come happenin
+	defer sc.mutex.Unlock() //	Unlocks mutex when function is complete
+	if time.Now().Sub(sc.expiration) < 0 {
+		return sc.cache, nil
 	}
-	stories, err := getTopStories(numStories)
+	stories, err := getTopStories(sc.numStories)
 	if err != nil {
 		return nil, err
 	}
-	cache = stories
-	cacheExpiration = time.Now().Add(15 * time.Minute) //	sets the cachexpiration to 15 minutes
-	return stories, nil
-
+	sc.expiration = time.Now().Add(sc.duration)
+	sc.cache = stories
+	return sc.cache, nil
 }
 
 func getTopStories(numStories int) ([]item, error) {
